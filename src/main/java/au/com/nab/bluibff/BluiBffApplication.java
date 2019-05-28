72512e536bb6b2bf547724a40f3f57502b2b5878
austiine04 <augustine@Augustines-MacBook-Pro.local>
2019-05-28 19:06:15 +1000

    first commit

---
+++ src/main/java/au/com/nab/bluibff/BluiBffApplication.java
@@ -0,0 +1,13 @@
+package au.com.nab.bluibff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluiBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluiBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/nab/bluibff/BluiBffApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.nab.bluibff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluiBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
